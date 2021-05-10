ba910d17bb3fa9aa48edfd32ef722deabba6c31c
dcep93 <dan@quizlet.com>
2021-05-10 00:40:26 -0700

    x

--- src/controls.ts
+++ src/controls.ts
@@ -1,15 +1,38 @@
 class Controls {
   auto_reply = document.getElementById("auto_reply") as HTMLInputElement;
+  is_active = false;
 
   constructor() {
-    document.getElementById("new_game").onclick = this.new_game.bind(this);
-    document.getElementById(
-      "different_move"
-    ).onclick = this.different_move.bind(this);
-    document.getElementById("undo").onclick = navigate.undo.bind(navigate);
-    document.getElementById("redo").onclick = navigate.redo.bind(navigate);
-    document.getElementById("reply").onclick = board.reply.bind(board);
-    document.getElementById("best").onclick = board.best.bind(board);
+    document.getElementById("new_game").onclick = this.queue(
+      this.new_game.bind(this)
+    );
+    document.getElementById("different_move").onclick = this.queue(
+      this.different_move.bind(this)
+    );
+    document.getElementById("undo").onclick = this.queue(
+      navigate.undo.bind(navigate)
+    );
+    document.getElementById("redo").onclick = this.queue(
+      navigate.redo.bind(navigate)
+    );
+    document.getElementById("reply").onclick = this.queue(
+      board.reply.bind(board)
+    );
+    document.getElementById("best").onclick = this.queue(
+      board.best.bind(board)
+    );
+  }
+
+  queue(f: () => void) {
+    return () => {
+      if (this.is_active) return;
+      try {
+        this.is_active = true;
+        f();
+      } finally {
+        this.is_active = false;
+      }
+    };
   }
 
   new_game() {
