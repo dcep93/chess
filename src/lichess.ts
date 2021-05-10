bd649b6b5d033e65e4f7f69ab935b797243b63a9
dcep93 <dan@quizlet.com>
2021-05-09 18:18:50 -0700

    x

--- src/lichess.ts
+++ src/lichess.ts
@@ -1,16 +1,16 @@
 class Lichess {
-  async _get_moves(fen: string): Promise<Move[]> {
-    console.log("lichess", fen);
-    const response = await fetch(
-      `https://explorer.lichess.ovh/master?fen=${fen}`
-    );
-    const json = await response.json();
-    return json.moves.map((m: any) => ({ move: m.san, ...m }));
-  }
-
   async get_moves(): Promise<Move[]> {
     const fen = board.chess.fen();
-    return (await cache_w.load(fen, () => this._get_moves(fen))).rval;
+    return (
+      await cache_w.load(fen, async () => {
+        console.log("lichess", fen);
+        const response = await fetch(
+          `https://explorer.lichess.ovh/master?fen=${fen}`
+        );
+        const json = await response.json();
+        return json.moves.map((m: any) => ({ move: m.san, ...m }));
+      })
+    ).rval;
   }
 }
 
