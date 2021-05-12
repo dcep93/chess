4e24aaeffdfa84a712d16d6d40b77ee800424e4e
dcep93 <dan@quizlet.com>
2021-05-11 19:05:26 -0700

    x

--- src/lichess.ts
+++ src/lichess.ts
@@ -7,11 +7,13 @@ class Lichess {
     console.log(url);
     const response = await fetch(url);
     const json = await response.json();
-    return json.moves.map((m: any) => ({
+    const moves = json.moves.map((m: any) => ({
       total: m.black + m.white + m.draws,
       move: m.san,
       ...m,
     }));
+    localStorage.setItem(url, JSON.stringify(moves));
+    return moves;
   }
 }
 
