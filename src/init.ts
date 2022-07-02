2ea6037af7df074c3037862587ff32b75c1cbb37
dcep93 <dan@quizlet.com>
2022-07-02 15:03:52 -0700

    x

--- src/init.ts
+++ src/init.ts
@@ -1,6 +1,7 @@
 function init() {
-  openings
-    .init()
+  console.log("version 1.1.0");
+  Promise.resolve()
+    .then(() => openings.init())
     .then(() => navigate.reset())
     .then(() => brain.on_change())
     .then(() => (document.body.hidden = false));
